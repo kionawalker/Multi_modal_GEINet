600251ddd3ca480c6d8a157c9911a5081f118ea6
kionawalker <35721387+kionawalker@users.noreply.github.com>
2018-02-02 13:18:46 +0900

    Add files via upload

---
+++ Multi_modal_GEINet_late_fusion).py
@@ -0,0 +1,56 @@
+import numpy as np
+import os
+import glob
+from PIL import Image
+import cv2
+import chainer
+import cupy as cp
+from chainer import Serializer, training, reporter, iterators, Function, cuda ,initializers
+from chainer import functions as F
+from chainer import links as L
+from chainer import Chain
+from chainer.training import extensions
+from chainer import serializers
+from chainer.dataset import convert
+from chainer.dataset import iterator as iterator_module
+from chainer.functions.evaluation import accuracy
+import six
+
+from sklearn.neighbors import KNeighborsClassifier
+import pandas as pd
+
+
+class Multi_modal_GEINet(Chain):
+    def __init__(self):
+        super(GEINet, self).__init__()
+        self.W = initializers.HeNormal()
+        self.b = initializers.Constant(fill_value=0)
+        with self.init_scope():
+            self.conv1 = L.ConvolutionND(ndim=2, in_channels=3, out_channels=18, ksize=7, stride=1, initialW=self.W, initial_bias=self.b)
+            self.conv2 = L.ConvolutionND(ndim=2, in_channels=18, out_channels=45, ksize=5, stride=1, pad=(2, 2), initialW=self.W, initial_bias=self.b)
+            self.fc3 = L.Linear(None, 2048, initialW=self.W, initial_bias=self.b)
+            self.fc4 = L.Linear(None, 956, initialW=self.W, initial_bias=self.b)
+            # L.Deconvolution2D()
+
+    def __call__(self, x, y):
+        root1_h1 = F.relu(self.conv1(x))
+        root1_h2 = F.max_pooling_2d(root1_h1, stride=2, ksize=2)
+        root1_h3 = F.local_response_normalization(root1_h2, k=1, n=5, alpha=0.00002, beta=0.75)
+        root1_h4 = F.relu(self.conv2(root1_h3))
+        root1_h5 = F.max_pooling_2d(root1_h4, stride=2, ksize=3)
+        root1_h6 = F.local_response_normalization(root1_h5, k=1, n=5, alpha=0.00002, beta=0.75)
+        root1_h7 = F.dropout(F.relu(self.fc3(root1_h6)), ratio=0.5)
+        
+        root2_h1 = F.relu(self.conv1(y))
+        root2_h2 = F.max_pooling_2d(root2_h1, stride=2, ksize=2)
+        root2_h3 = F.local_response_normalization(root2_h2, k=1, n=5, alpha=0.00002, beta=0.75)
+        root2_h4 = F.relu(self.conv2(root2_h3))
+        root2_h5 = F.max_pooling_2d(root2_h4, stride=2, ksize=3)
+        root2_h6 = F.local_response_normalization(root2_h5, k=1, n=5, alpha=0.00002, beta=0.75)
+        root2_h7 = F.dropout(F.relu(self.fc3(root2_h6)), ratio=0.5)
+
+        h7 = F.concat((root1_h7, root2_h7), axis=1)
+        
+        h8 = self.fc4(h7)
+        # h9 = F.softmax(h8)
+        return h8
---
+++ Multi_modal_GEINet_middle_test.py
@@ -0,0 +1,99 @@
+# coding:utf-8
+
+import numpy as np
+import os
+import glob
+from PIL import Image
+import cv2
+import chainer
+import cupy as cp
+from chainer import Serializer, training, reporter, iterators, Function, cuda ,initializers
+from chainer import functions as F
+from chainer import links as L
+from chainer import Chain
+from chainer.training import extensions
+from chainer import serializers
+from chainer.dataset import convert
+from chainer.dataset import iterator as iterator_module
+from chainer.functions.evaluation import accuracy
+import six
+import sys
+sys.path.append('/home/common/setoguchi/chainer_files/SFDEINet_multi_modal/Multi_modal_GEINet-master')
+from tools import load_GEI
+sys.path.append('/home/common/setoguchi/chainer_files/SFDEINet_multi_modal/Multi_modal_GEINet-master')
+from Multi_modal_GEINet_middle_fusion import Multi_modal_GEINet
+from sklearn.neighbors import KNeighborsClassifier
+
+def extract_features(model, data1, data2):
+    feature_list = np.empty((0, 1024), dtype=float)
+    # count = 0
+    for i in range(len(data1)):
+        # Forward処理を記述
+        root1_h1 = F.relu(model.conv1(data1[i]))
+        root1_h2 = F.max_pooling_2d(root1_h1, stride=2, ksize=2)
+        root1_h3 = F.local_response_normalization(root1_h2, k=1, n=5, alpha=0.00002, beta=0.75)
+        root1_h4 = F.relu(model.conv2(root1_h3))
+        root1_h5 = F.max_pooling_2d(root1_h4, stride=2, ksize=3)
+        root1_h6 = F.local_response_normalization(root1_h5, k=1, n=5, alpha=0.00002, beta=0.75)
+
+        root2_h1 = F.relu(model.conv1(data2[i]))
+        root2_h2 = F.max_pooling_2d(root2_h1, stride=2, ksize=2)
+        root2_h3 = F.local_response_normalization(root2_h2, k=1, n=5, alpha=0.00002, beta=0.75)
+        root2_h4 = F.relu(model.conv2(root2_h3))
+        root2_h5 = F.max_pooling_2d(root2_h4, stride=2, ksize=3)
+        root2_h6 = F.local_response_normalization(root2_h5, k=1, n=5, alpha=0.00002, beta=0.75)
+
+        h6 = F.concat((root1_h6, root2_h6), axis=1)
+
+        feature = F.relu(model.fc3(h6))
+        # h8 = model.fc4(h7)
+
+        # print("fc3層の出力は：" + str(feature.data[0]))
+        feature_list = np.append(feature_list, np.array([feature.data[0]]), axis=0)
+    # feature_list = feature_list.flatten
+
+
+    return feature_list
+
+
+def recognition(model_name):
+    # 識別用のデータをダウンロード
+    train1, train_labels1 = load_GEI(
+        "/home/common/setoguchi/chainer_files/dataset/SFDEI/CV02(Gallery)_2nd",
+        mode=False)
+    train2, train_labels2 = load_GEI(
+        "/home/common/setoguchi/chainer_files/dataset/SFDEI/CV02_Dt2(Gallery)",
+        mode=False)
+    test1, test_labels1 = load_GEI(
+        "/home/common/setoguchi/chainer_files/dataset/SFDEI/CV02(Probe)_2nd",
+        mode=False)
+    test2, test_labels2 = load_GEI(
+        "/home/common/setoguchi/chainer_files/dataset/SFDEI/CV02_Dt2(Probe)",
+        mode=False)
+
+
+
+    # extract features
+    model = Multi_modal_GEINet()
+    serializers.load_npz(model_name, obj=model)
+
+    train_features = extract_features(model, train1, train2)
+    test_features = extract_features(model, test1, test2)
+
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    neigh.fit(train_features, train_labels1)
+
+    correct = 0.0
+    for i, item in enumerate(test_features):
+        predit = neigh.predict([item])[0]
+        print "label:%d, predict:%d" % (test_labels1[i], predit)
+        if predit == test_labels1[i]:
+            correct = correct + 1
+    # else:
+    #         f.write("label:%d, predict:%d" %(true_label[i],predit)+"\n")
+
+    acc = correct / len(test_features)
+    print acc
+
+if __name__=='__main__':
+    recognition('/home/common/setoguchi/chainer_files/SFDEINet_multi_modal/Multi_modal_GEINet-master/result_middle/model_snapshot_6250')
